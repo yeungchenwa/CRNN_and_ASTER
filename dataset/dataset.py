d952f05f03ecf945ae80e72d63eee70351dd3eef
yeungchenwa <523709335@qq.com>
2022-07-09 16:48:13 +0800

    add dataloader pipeline

---
+++ dataset/dataset.py
@@ -0,0 +1,197 @@
+import sys
+import re
+import six
+import lmdb
+import torch
+import argparse
+import random
+
+from PIL import Image
+import numpy as np
+from torch.utils.data import Dataset
+import torchvision.transforms as transforms
+from torch.utils.data import sampler
+
+class LmdbDataset(Dataset):
+
+    def __init__(self, root, opt):
+
+        self.root = root
+        self.opt = opt
+        self.env = lmdb.open(root, max_readers=32, readonly=True, lock=False, readahead=False, meminit=False)
+        # print(self.env)
+        if not self.env:
+            print('cannot create lmdb from %s' % (root))
+            sys.exit(0)
+
+        with self.env.begin(write=False) as txn:
+            nSamples = int(txn.get('num-samples'.encode()))
+            self.nSamples = nSamples
+
+            if self.opt.data_filtering_off:
+                # for fast check or benchmark evaluation with no filtering
+                self.filtered_index_list = [index + 1 for index in range(self.nSamples)]
+            else:
+                """ Filtering part
+                If you want to evaluate IC15-2077 & CUTE datasets which have special character labels,
+                use --data_filtering_off and only evaluate on alphabets and digits.
+                see https://github.com/clovaai/deep-text-recognition-benchmark/blob/6593928855fb7abb999a99f428b3e4477d4ae356/dataset.py#L190-L192
+
+                And if you want to evaluate them with the model trained with --sensitive option,
+                use --sensitive and --data_filtering_off,
+                see https://github.com/clovaai/deep-text-recognition-benchmark/blob/dff844874dbe9e0ec8c5a52a7bd08c7f20afe704/test.py#L137-L144
+                """
+                self.filtered_index_list = []
+                for index in range(self.nSamples):
+                    index += 1  # lmdb starts with 1
+                    label_key = 'label-%09d'.encode() % index
+                    label = txn.get(label_key).decode('utf-8')
+
+                    if len(label) > self.opt.batch_max_length:
+                        # print(f'The length of the label is longer than max_length: length
+                        #       {len(label)}, {label} in dataset {self.root}')
+                        continue
+
+                    # By default, images containing characters which are not in opt.character are filtered.
+                    # You can add [UNK] token to `opt.character` in utils.py instead of this filtering.
+                    out_of_char = f'[^{self.opt.character}]'
+                    # out_of_char = f'[^{self.opt.character}]'
+                    if re.search(out_of_char, label.lower()):
+                        continue
+
+                    self.filtered_index_list.append(index)
+
+                self.nSamples = len(self.filtered_index_list)
+
+    def __len__(self):
+        return self.nSamples
+
+    def __getitem__(self, index):
+        assert index <= len(self), 'index range error'
+        index = self.filtered_index_list[index]
+
+        with self.env.begin(write=False) as txn:
+            label_key = 'label-%09d'.encode() % index
+            label = txn.get(label_key).decode('utf-8')
+            img_key = 'image-%09d'.encode() % index
+            imgbuf = txn.get(img_key)
+
+            buf = six.BytesIO()
+            buf.write(imgbuf)
+            buf.seek(0)
+            try:
+                if self.opt.rgb:
+                    img = Image.open(buf).convert('RGB')  # for color image
+                else:
+                    img = Image.open(buf).convert('L')
+
+            except IOError:
+                print(f'Corrupted image for {index}')
+                # make dummy image and dummy label for corrupted image.
+                if self.opt.rgb:
+                    img = Image.new('RGB', (self.opt.imgW, self.opt.imgH))
+                else:
+                    img = Image.new('L', (self.opt.imgW, self.opt.imgH))
+                label = '[dummy_label]'
+
+            if not self.opt.sensitive:
+                label = label.lower()
+
+            # We only train and evaluate on alphanumerics (or pre-defined character set in train.py)
+            out_of_char = f'[^{self.opt.character}]'
+            print(label)
+            label = re.sub(out_of_char, '', label)
+            # print('0')
+            # print(label)
+
+        return (img, label)
+    
+class AlignCollate(object):
+
+    def __init__(self, imgH=32, imgW=100, keep_ratio=False, min_ratio=1):
+        self.imgH = imgH
+        self.imgW = imgW
+        self.keep_ratio = keep_ratio
+        self.min_ratio = min_ratio
+
+    def __call__(self, batch):
+        images, labels, lengths = zip(*batch)
+        b_lengths = torch.IntTensor(lengths)
+        b_labels = torch.IntTensor(labels)
+
+        imgH = self.imgH
+        imgW = self.imgW
+        if self.keep_ratio:
+            ratios = []
+            for image in images:
+                w, h = image.size
+                ratios.append(w / float(h))
+            ratios.sort()
+            max_ratio = ratios[-1]
+            imgW = int(np.floor(max_ratio * imgH))
+            imgW = max(imgH * self.min_ratio, imgW)  # assure imgH >= imgW
+            imgW = min(imgW, 400)
+
+        transform = ResizeNormalize((imgW, imgH))
+        images = [transform(image) for image in images]
+        b_images = torch.stack(images)
+
+        return b_images, b_labels, b_lengths
+
+class ResizeNormalize(object):
+  def __init__(self, size, interpolation=Image.BILINEAR):
+    self.size = size
+    self.interpolation = interpolation
+    self.toTensor = transforms.ToTensor()
+
+  def __call__(self, img):
+    img = img.resize(self.size, self.interpolation)
+    img = self.toTensor(img)
+    img.sub_(0.5).div_(0.5)
+    return img
+
+class RandomSequentialSampler(sampler.Sampler):
+
+  def __init__(self, data_source, batch_size):
+    self.num_samples = len(data_source)
+    self.batch_size = batch_size
+
+  def __len__(self):
+    return self.num_samples
+
+  def __iter__(self):
+    n_batch = len(self) // self.batch_size
+    tail = len(self) % self.batch_size
+    index = torch.LongTensor(len(self)).fill_(0)
+    for i in range(n_batch):
+      random_start = random.randint(0, len(self) - self.batch_size)
+      batch_index = random_start + torch.arange(0, self.batch_size)
+      index[i * self.batch_size:(i + 1) * self.batch_size] = batch_index
+    # deal with tail
+    if tail:
+      random_start = random.randint(0, len(self) - self.batch_size)
+      tail_index = random_start + torch.arange(0, tail)
+      index[(i + 1) * self.batch_size:] = tail_index
+
+    return iter(index.tolist())
+
+if __name__ == '__main__':
+    device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--batch_max_length', type=int, default=25, help='maximum-label-length') # T的大小
+    parser.add_argument('--imgH', type=int, default=32, help='the height of the input image')
+    parser.add_argument('--imgW', type=int, default=100, help='the width of the input image')
+    parser.add_argument('--rgb', action='store_true', help='use rgb input')
+    parser.add_argument('--character', type=str,
+                        default='0123456789abcdefghijklmnopqrstuvwxyz', help='character label')  # 关键点
+    parser.add_argument('--sensitive', action='store_true', help='for sensitive character mode') # 能区分大写小写
+    parser.add_argument('--data_filtering_off', action='store_true', help='for data_filtering_off mode')  # ？
+    parser.add_argument('--PAD', action='store_true', help='whether to keep ratio then pad for image resize')
+    opt = parser.parse_args()
+
+    data = LmdbDataset('E:/DLVC/Datasets/STR/data_lmdb_release/validation', opt=opt)
+    img, label = data.__getitem__(0)
+    print(len(data))
+    print(img)
+    print(label)
---
+++ dataset/utils.py
@@ -0,0 +1,67 @@
+from lib2to3.pytree import convert
+import os
+import argparse
+
+from dataset import LmdbDataset, RandomSequentialSampler, AlignCollate
+from concatdataset import ConcatDataset
+import torch
+from torch.utils.data import DataLoader
+from tools import CTCLabelConverter
+
+def get_data(data_dir, opt, is_train, randomSampler=False):
+    if isinstance(data_dir, list):
+        dataset_list = []
+        for data_dir_ in data_dir:
+            print(data_dir_)
+            dataset_list.append(LmdbDataset(data_dir_, opt))
+        dataset = ConcatDataset(dataset_list)
+    else:
+        dataset = LmdbDataset(data_dir, opt)
+    print('total image: ', len(dataset))
+    # print(dataset_list)
+
+    if randomSampler:        # caiyang
+        sampler = RandomSequentialSampler(dataset, batch_size=opt.batch_size)
+        shuffle = False
+    else:
+        sampler = None
+        shuffle = True
+
+    if is_train:
+        data_loader = DataLoader(dataset, batch_size=opt.batch_size, num_workers=opt.num_workers,
+                                shuffle=shuffle, pin_memory=True, drop_last=True,sampler=sampler,
+                                collate_fn=AlignCollate(imgH=opt.imgH, imgW=opt.imgW))
+    else:
+        data_loader = DataLoader(dataset, batch_size=opt.batch_size, num_workers=opt.num_workers,
+                                shuffle=True, pin_memory=True, drop_last=False, 
+                                collate_fn=AlignCollate(imgH=opt.imgH, imgW=opt.imgW))
+
+    return dataset, data_loader
+
+if __name__ == "__main__":
+    device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--batch_max_length', type=int, default=25, help='maximum-label-length') # T的大小
+    parser.add_argument('--batch_size', type=int, default=4, help='the batch size of each epoch')
+    parser.add_argument('--num_workers', type=int, default=0)
+    parser.add_argument('--imgH', type=int, default=32, help='the height of the input image')
+    parser.add_argument('--imgW', type=int, default=100, help='the width of the input image')
+    parser.add_argument('--rgb', action='store_true', help='use rgb input')
+    parser.add_argument('--character', type=str,
+                        default='0123456789abcdefghijklmnopqrstuvwxyz', help='character label')  # 关键点
+    parser.add_argument('--sensitive', action='store_true', help='for sensitive character mode') # 能区分大写小写
+    parser.add_argument('--data_filtering_off', action='store_true', help='for data_filtering_off mode')  # ？
+    parser.add_argument('--PAD', action='store_true', help='whether to keep ratio then pad for image resize')
+    opt = parser.parse_args()
+
+    root = 'E:\DLVC\Datasets\STR\scene_text_train'
+    data_dir = [os.path.join(root, dir_name) for dir_name in os.listdir(root)]
+    print(data_dir)
+    dataset, data_loader = get_data(data_dir, opt=opt, is_train=True, randomSampler=True)
+    print(len(dataset))
+
+    # converter = CTCLabelConverter(opt.character)
+    # text, length = converter.encode(label, batch_max_length=opt.batch_max_length)
+    # print(text.size())
+    # print(text)
